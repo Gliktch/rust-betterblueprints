816a35e7ca99f18ae3ea6e81b0facc86d0128040
Matthew (Gliktch) <github-07IU02@gliktch.com>
2015-12-24 23:06:19 +1000

    Create BetterBlueprints.cs
    Initial dummy commit to test Atom integration

---
+++ BetterBlueprints.cs
@@ -0,0 +1,7 @@
+// Initial dummy commit to test Atom integration
+
+private void OnPlayerLoot(PlayerLoot lootInventory, BaseEntity targetEntity)
+{
+	if(targetEntity is ResearchTable)
+		// do a thing;
+}
